ea268bd859fcf508147ce3823ecaf80077814ee0
BrianReisman <brianreisman@protonmail.com>
2021-04-11 18:42:57 -0400

    deploy 20210411 after removing linting errors in Work.js

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import StyledWork from "./StyledWork";
 import WorkContainer from "./WorkContainer/WorkContainer";
 
 const Work = (props) => {
-  const [showExtraProjects, setShowExtraProjects] = useState(false);
+  // const [showExtraProjects, setShowExtraProjects] = useState(false);
 
-  const clickHandler = (e) => {
-    setShowExtraProjects(!showExtraProjects);
-  };
+  // const clickHandler = (e) => {
+  //   setShowExtraProjects(!showExtraProjects);
+  // };
 
   return (
     <StyledWork>
       <h2 id="work">Work</h2>
-      <WorkContainer showExtraProjects={showExtraProjects} />
+      <WorkContainer />
+      {/* <WorkContainer showExtraProjects={showExtraProjects} /> */}
       {/* buttons */}
       {/* {showExtraProjects ?
       <button onClick={clickHandler}>Get those extra projects outta here!</button>
