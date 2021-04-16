dee6f0f2d6f8bee15dc0eccff63986a5b5d1f2e6
BrianReisman <brianreisman@protonmail.com>
2021-04-16 10:54:02 -0400

    deploy 20210416

--- src/components/util/Spacer/StyledSpacer.js
+++ src/components/util/Spacer/StyledSpacer.js
@@ -4,5 +4,9 @@ const StyledSpacer = styled.div`
   background-color: ${props => props.theme.color.transition};
   height: 50px;
   box-sizing: border-box;
+
+  @media (max-width: ${props => props.theme.breakpoints.mid}){
+    height: 30px;
+  }
 `;
 export default StyledSpacer;
